218935bc4f702e40a4c03071bfe5b9c58296d2cd
Judy Jow <judy@socialtables.com>
2014-10-04 13:13:04 -0400

    WIP header, projects index page

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,5 +1,10 @@
-class ProjectController < ActionController::Base
+class ProjectController < ApplicationController
   def home
   	render :home
+  end
+
+  def index
+  	@projects = Project.all
+  	render :index
   end
 end
