91bf0a14cc5c6468ccf65351d25e23682c45d1fc
Judy Jow <judy@socialtables.com>
2014-10-04 13:19:35 -0400

    Update naming for Project to Projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "project#home"
+  root to: "projects#home"
 
   resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
